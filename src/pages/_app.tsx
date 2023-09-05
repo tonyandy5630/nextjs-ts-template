278af62e5fd7ae929893c4e85599816f364561e3
tonyandy5630 <tonyandy456@gmail.com>
2023-09-05 14:26:20 +0700

    add glbalcsspriority and global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,23 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 const queryClient = new QueryClient()
+import { ThemeProvider } from '@mui/material'
+import GlobalCssPriority from '@/components/globalcssprop'
+import theme from '@/utils/theme'
+import { Provider } from 'react-redux'
+import { store } from '@/store'
+import '@/app/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
+            <Provider store={store}>
+                <ThemeProvider theme={theme}>
+                    <GlobalCssPriority>
+                        <Component {...pageProps} />
+                    </GlobalCssPriority>
+                </ThemeProvider>
+            </Provider>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     )
